dc7584698b37f6451d3b961d5fdc8d09eff3b18e
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-10-09 02:39:47 +1300

    completed boolean lesson

--- C++ Curse/5. Booleans/main.cpp
+++ C++ Curse/5. Booleans/main.cpp
@@ -1,20 +1,23 @@
-// 5. Booleans.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool redLight{ false };
+    bool greenLight{ true };
+
+    if (redLight == true) {
+        std::cout << "Stop!" << std::endl;
+    } else {
+        std::cout << "Go through" << std::endl;
+    }
+
+    if (greenLight) {
+        std::cout << "The light is green!" << std::endl;
+    } else {
+        std::cout << "The light is NOT green!" << std::endl;
+    }
+
+    // Printing out a bool
+    // 1 -> true
+    // 0 -> false
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
